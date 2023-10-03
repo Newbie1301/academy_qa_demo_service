e22eab3d3ae36f4adc8316963781acb6f16beb10
Newbie1301 <146757964+Newbie1301@users.noreply.github.com>
2023-10-03 22:27:25 +0300

    Update config.py
    ...

--- tasks/config.py
+++ tasks/config.py
@@ -2,12 +2,12 @@ import os
 
 PACKAGE_VERSION = "0.0.0"
 with open(os.path.join("./version"), mode="r") as version_file:
-    PACKAGE_VERSION = version_file.readline()
+    PACKAGE_VERSION = version_file.("readline")
 
 SERVICE_NAME = "academy_qa_demo_service"
 GENERATED_PATH = "./generated"
 
 DOCKER_IMAGE_NAME = SERVICE_NAME
-DOCKER_REGISTRY_PASSWORD = os.environ.get("DOCKERHUB_PASSWORD", "")
-DOCKER_REGISTRY_USER = os.environ.get("DOCKERHUB_USERNAME", "")
+DOCKER_REGISTRY_PASSWORD = os.environ.get("DOCKERHUB_PASSWORD","")
+DOCKER_REGISTRY_USER = os.environ.get("DOCKERHUB_USERNAME","")
 DOCKER_IMAGE_REGISTRY = "docker.hub.com"
